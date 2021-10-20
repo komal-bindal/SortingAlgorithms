ab46d1b661b9f1bb3cc9aba27512b1735ed4950a
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-10-20 14:32:13 +0530

    add insertion sort algorithm

---
+++ src/InsertionSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(insertionSort(arr)));
+    }
+
+    /**
+     * Insertion Sort - It is stable and in-place algorithm.
+     * Time Complexity - O(n^2)
+     * Space Complexity - O(1) -> (in-place)
+     *
+     * @param arr - array to be sorted
+     * @return arr - sorted array
+     */
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+}
